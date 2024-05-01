bb43f3894ab556e9d2019d50242ab6e0d70d6d09
mqurban <145771370+mqurban@users.noreply.github.com>
2024-05-01 23:43:47 +0500

    Time: 138 ms (25.9%), Space: 17 MB (12.3%) - LeetHub

--- 0605-can-place-flowers/0605-can-place-flowers.py
+++ 0605-can-place-flowers/0605-can-place-flowers.py
@@ -1,18 +1,8 @@
 class Solution:
     def canPlaceFlowers(self, flowerbed, n):
-        flowerbed.insert(0, 0)
-        flowerbed.append(0)
-        
-        flowerbed_length = len(flowerbed)
-        i = 1
-        while i < flowerbed_length - 1:
-            if flowerbed[i] + flowerbed[i - 1] + flowerbed[i + 1] == 0:
+        f = [0] + flowerbed + [0]
+        for i in range(1, len(f) - 1):
+            if f[i - 1] == 0 and f[i] == 0 and f[i + 1] == 0:
+                f[i] = 1
                 n -= 1
-                i += 1
-
-            if n <= 0:
-                return True
-            
-            i += 1
-        
         return n <= 0
